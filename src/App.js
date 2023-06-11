6af8a8c4050517e4a16f71ef92fe4e092af1fa04
Souvick Chakraborty <hellosouvickk@gmail.com>
2023-06-11 13:46:14 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([
+    { id: 1, img: 'images/download.jpeg' },
+    { id: 2, img: 'images/download.jpeg' },
+    { id: 3, img: 'images/download.jpeg' },
+    { id: 4, img: 'images/download.jpeg' },
+  ]);
+
+  const [droppedItems, setDroppedItems] = useState([]);
+
+  const handleDragStart = (e, item) => {
+    e.dataTransfer.setData('text/plain', JSON.stringify(item));
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const item = JSON.parse(e.dataTransfer.getData('text/plain'));
+    setDroppedItems((prevItems) => [...prevItems, item]);
+  };
+
+  const handleReset = () => {
+    setItems([
+      { id: 1, img: 'images/download.jpeg' },
+      { id: 2, img: 'images/download.jpeg' },
+      { id: 3, img: 'images/download.jpeg' },
+      { id: 4, img: 'images/download.jpeg' },
+    ]);
+    setDroppedItems([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className='Item'>
+        <div className="container">
+          {items.map((item) => (
+            <div
+              key={item.id}
+              className="item"
+              draggable="true"
+              onDragStart={(e) => handleDragStart(e, item)}
+            >
+              <img src={item.img} alt="" />
+            </div>
+          ))}
+        </div>
+        <div
+          className="container"
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {droppedItems.length === 0 ? (
+            <p>Drop items here</p>
+          ) : (
+            droppedItems.map((item) => (
+              <div key={item.id} className="item">
+                <img src={item.img} alt="" />
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+      <button className="reset-button" onClick={handleReset}>Reset</button>
+    </>
   );
-}
+};
 
 export default App;
